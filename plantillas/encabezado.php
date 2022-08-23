2bf5e5b09633775206b2f2cdfecbd85b8f6f46d3
JoeTech-Studio <joevelez_1234@outlook.es>
2022-08-23 12:26:54 -0500

    ACTUALIZADO

--- plantillas/encabezado.php
+++ plantillas/encabezado.php
@@ -3,8 +3,8 @@
         <?php echo isset($titulo)?$titulo:""; ?>
     </h1>
     <nav>
-        <a href="/gym/modulo/producto/consultar.php">Producto</a>
-        <a href="/gym/modulo/cliente/consultar.php">Cliente</a>
+        <a href="/daw-gym-BDD/modulo/producto/consultar.php">Producto</a>
+        <a href="/daw-gym-BDD/modulo/cliente/consultar.php">Cliente</a>
 
     </nav>
 </div>
