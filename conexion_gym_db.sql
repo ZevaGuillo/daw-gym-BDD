d6c9b07ed96bcaf66bdff970a7bcb60a57abed76
JoeTech-Studio <joevelez_1234@outlook.es>
2022-08-25 19:37:20 -0500

    ACTUALIZADO

--- conexion_gym_db.sql
+++ conexion_gym_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-08-2022 a las 01:36:30
+-- Tiempo de generación: 26-08-2022 a las 02:36:45
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.0.19
 
@@ -61,7 +61,8 @@ INSERT INTO `articulo` (`id`, `art_nombre`, `nombre`, `apellido`, `cedula`, `cor
 (19, 'mancuerna', 'FSSFSDFS', 'Marcillo', 0, 'nandocse', 'dsdsddsdsds'),
 (20, 'mancuerna', 'FSSFSDFS', 'Marcillo', 955188941, 'nandocse', 'dsdsddsdsds'),
 (21, 'mancuerna', 'FSSFSDFS', 'Marcillo', 955188941, 'nandocse', 'dsdsddsdsds'),
-(22, 'mancuerna', 'reteterteter', 'trtertertrtr', 0, 'rtrtertretret', 'tertetrtertret');
+(22, 'mancuerna', 'reteterteter', 'trtertertrtr', 0, 'rtrtertretret', 'tertetrtertret'),
+(0, 'mancuerna', 'PACMAN', 'asdf', 2134, 'sf', 'sdf');
 
 -- --------------------------------------------------------
 
@@ -93,12 +94,12 @@ INSERT INTO `cliente` (`id`, `nombre`, `telefono`, `email`, `asunto`, `mensaje`)
 --
 
 CREATE TABLE `nosotros` (
-  `ID_REGISTRO` int(12) NOT NULL,
+  `id` int(12) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `Correo` varchar(100) NOT NULL,
-  `PagoMensual` int(20) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `pagoMensual` int(20) NOT NULL,
   `fecha` date NOT NULL,
-  `ObjetivosGym` varchar(50) NOT NULL
+  `objetivos` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -121,19 +122,45 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`prd_id`, `prd_nombre`, `prd_valor`, `prd_cantidad`, `prd_estado`, `prd_codigo_proveedor_producto`) VALUES
-(1, 'Jugo', 5, 4, '1', '100'),
-(2, 'Jugo', 1, 3, '1', '100'),
-(3, 'Jugo', 4, 7, '1', '100'),
-(4, 'Jugo', 5, 7, '1', '100');
+(2, 'Joe', 2, 1, '1', '100');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `suscripcion`
+--
+
+CREATE TABLE `suscripcion` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `apellido` varchar(60) NOT NULL,
+  `edad` int(3) NOT NULL,
+  `genero` varchar(60) NOT NULL,
+  `plan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `suscripcion`
+--
+
+INSERT INTO `suscripcion` (`id`, `nombre`, `apellido`, `edad`, `genero`, `plan`) VALUES
+(1, 'Joe', '0', 21, 'Masculino', 'MENSUAL'),
+(2, 'Joe', 'Velez', 21, 'Masculino', 'ANUAL');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `articulo`
+-- Indices de la tabla `cliente`
 --
-ALTER TABLE `articulo`
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `nosotros`
+--
+ALTER TABLE `nosotros`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -143,20 +170,38 @@ ALTER TABLE `producto`
   ADD PRIMARY KEY (`prd_id`);
 
 --
+-- Indices de la tabla `suscripcion`
+--
+ALTER TABLE `suscripcion`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `articulo`
+-- AUTO_INCREMENT de la tabla `cliente`
 --
-ALTER TABLE `articulo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `nosotros`
+--
+ALTER TABLE `nosotros`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `prd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `prd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `suscripcion`
+--
+ALTER TABLE `suscripcion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
